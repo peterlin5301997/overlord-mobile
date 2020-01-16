e32c96cf94e0bf7c4face73610c9a6478282cfb3
Peter Lin <peterlin5301997@gmail.com>
2020-01-16 12:10:01 -0500

    adds handlePress, compares choices and returns result

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,13 +3,35 @@ import { StyleSheet, View, Text, Button } from 'react-native';
 
 class GameScreen extends React.Component {
 
+  state = {
+    playerChoice: '',
+    cpuChoice: '',
+  }
+
+  handlePress = (playerChoice) => {
+    const choices = ['Rock','Paper','Scissor']
+    const cpuChoice = choices[Math.floor(Math.random() * choices.length)]
+    this.setState({ cpuChoice, playerChoice })
+
+    let result = playerChoice == cpuChoice ? 'TIE' :
+    playerChoice == 'Rock' && cpuChoice == 'Paper' ? 'YOU LOSE' :
+    playerChoice == 'Rock' && cpuChoice == 'Scissor' ? 'YOU WIN' :
+    playerChoice == 'Paper' && cpuChoice == 'Rock' ? 'YOU WIN' :
+    playerChoice == 'Paper' && cpuChoice == 'Scissor' ? 'YOU LOSE' :
+    playerChoice == 'Scissor' && cpuChoice == 'Paper' ? 'YOU WIN' :
+    playerChoice == 'Scissor' && cpuChoice == 'Rock' ? 'YOU LOSE' : null
+
+    // console.log(result)
+  }
+
   render() {
+    // console.log(this.state)
     return(
       <View>
         <Text style={styles.title}>GameScreen</Text>
-        <View style={styles.button}><Button title='Rock     🤘' /></View>
-        <View style={styles.button}><Button title='Paper     🧻' /></View>
-        <View style={styles.button}><Button title='Scissor     ✂️' /></View>
+        <View style={styles.button}><Button onPress={() => this.handlePress('Rock')} title='Rock     🤘' /></View>
+        <View style={styles.button}><Button onPress={() => this.handlePress('Paper')} title='Paper     🧻' /></View>
+        <View style={styles.button}><Button onPress={() => this.handlePress('Scissor')} title='Scissor     ✂️' /></View>
       </View>
     )
   }
